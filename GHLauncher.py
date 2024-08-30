9ed5c5e9fd73e13f661b28258474294bfdafbb41
z <zd.ztyler@gmail.com>
2024-08-30 13:08:50 -0500

    VIEWPORT SIZE BY OS

--- GHLauncher.py
+++ GHLauncher.py
@@ -1,16 +1,22 @@
 import dearpygui.dearpygui as dpg
+import platform 
+
+class globals:
+    if platform.system() == "Darwin":
+        VIEWPORT_WIDTH = 1000
+        VIEWPORT_HEIGHT = 600
+    elif platform.system() == "Windows":
+        VIEWPORT_WIDTH = 1250
+        VIEWPORT_HEIGHT = 850
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
+dpg.create_viewport(title='Custom Title', width=1000, height=600)
 
 with dpg.window(label="Example Window"):
     dpg.add_text("Hello, world")
     dpg.add_button(label="Save")
     dpg.add_input_text(label="string", default_value="Quick brown fox")
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
-    dpg.add_text("-----------------------") 
-    #THIS IS THE START OF TALANS SECTION 
-
 
 
 
