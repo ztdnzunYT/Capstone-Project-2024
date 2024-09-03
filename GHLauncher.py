4fbe7f4895a3b1231f2cc04d811dbb91c779ceb3
Z Dunson <123149208+ztdnzunYT@users.noreply.github.com>
2024-09-03 09:23:38 -0500

    Update GHLauncher.py

--- GHLauncher.py
+++ GHLauncher.py
@@ -1,1 +1,28 @@
-"This is Mathis"
+import dearpygui.dearpygui as dpg
+import platform 
+
+
+class globals:
+    if platform.system() == "Darwin":
+        VIEWPORT_WIDTH = 1000
+
+dpg.create_context()
+dpg.create_viewport(title='GHLauncher', width=globals.VIEWPORT_WIDTH, height=globals.VIEWPORT_HEIGHT)
+
+dpg.set_global_font_scale(1.2)
+with dpg.window(pos=(0,0),width=globals.VIEWPORT_WIDTH,height=globals.VIEWPORT_HEIGHT,no_move=True,no_title_bar=True,no_resize=True,no_bring_to_front_on_focus=True) as background_window:
+    dpg.create_context()
+    with dpg.window(pos=(0,0),width=220,height=globals.VIEWPORT_HEIGHT,no_move=True,no_title_bar=True,no_resize=True)as side_menu:
+        dpg.add_text("GHLauncher")
+        dpg.add_separator()
+        dpg.add_spacer(height=30)
+        dpg.add_button(label="WHAT'S NEW")
+        dpg.add_button(label="CATELOG")
+        dpg.add_button(label="VISIT OUR WEBSITE")
+        dpg.add_button(label="SETTINGS")
+
+
+dpg.show_style_editor()
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
