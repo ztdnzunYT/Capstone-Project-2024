7459640ee4936deace1075ce65bd112826109243
z <zd.ztyler@gmail.com>
2024-09-05 21:59:23 -0500

    fixed name error

--- GHLauncher.py
+++ GHLauncher.py
@@ -24,13 +24,11 @@ class Thumbnail_props:
         self.channels = self.load_image[2]
         self.data = self.load_image[3]
         
-
 thumbnails = []
-kart_shifters_thumbnail = Thumbnail_props("Kart-Shifters-Poster-1.png.png","pic1","Kart Shifters")
+kart_shifters_thumbnail = Thumbnail_props("Kart-Shifters-Poster-1.png","pic1","Kart Shifters")
 basketball_thumbnail = Thumbnail_props("Screen-Shot-2024-03-05-at-10.50.37-AM.png","pic2","Stick Basketball")
 bbal_thumbnail = Thumbnail_props("bball-thumbnail-1.png","pic","B-Ball")
 thumbnails.extend([kart_shifters_thumbnail,basketball_thumbnail,bbal_thumbnail])
-
 
 dpg.set_global_font_scale(1.2)
 with dpg.window(pos=(0,0),width=globals.VIEWPORT_WIDTH,height=globals.VIEWPORT_HEIGHT,no_move=True,no_title_bar=True,no_resize=True,no_bring_to_front_on_focus=True) as background_window:
@@ -49,6 +47,9 @@ with dpg.window(pos=(0,0),width=globals.VIEWPORT_WIDTH,height=globals.VIEWPORT_H
         for thumbnail in thumbnails:
             dpg.add_static_texture(width=thumbnail.width,height=thumbnail.height,default_value=thumbnail.data,tag=thumbnail.tag)
 
+    def openApp(sender, app_data, user_data):
+        os.system(f"open {user_data}")
+
     with dpg.child_window(label="WHAT'S NEW",pos=(220,0),tag="whats_new_window") as whats_new_window:
         dpg.add_spacer(height=20)
         with dpg.group(horizontal=True):
@@ -57,26 +58,13 @@ with dpg.window(pos=(0,0),width=globals.VIEWPORT_WIDTH,height=globals.VIEWPORT_H
                 with dpg.group():
                     dpg.add_image(thumbnail.tag,width=150,height=200)
                     dpg.add_spacer(height=10)                    
-                    dpg.add_button(label=thumbnail.game_name,user_data=thumbnail.path,callback=lambda:os.system(f"open Kart-Shifters-Poster-1.png.png"))
+                    dpg.add_button(label=thumbnail.game_name,callback=openApp,user_data=thumbnail.path)
                    
         dpg.add_spacer(height=20)
         dpg.add_separator()
     
         
-
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
 
-"""
-
-import dearpygui.dearpygui as dpg
-
-dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=800, height=600)
-
-
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()"""
