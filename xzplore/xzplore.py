b86100206f825e94bc81c0f8d4345b00a0b1d586
z <zd.ztyler@gmail.com>
2024-09-15 13:31:50 -0500

    stars added

--- xzplore/xzplore.py
+++ xzplore/xzplore.py
@@ -21,7 +21,6 @@ class World_pos():
         mtos_dis = round(math.sqrt((mouse_pos[1]-spaceship[1])**2+(mouse_pos[0]-spaceship[0])**2))
         return mtos_dis
 
-
     x_offset = .1
     y_offset = .1
 
@@ -52,7 +51,7 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect(center=(x,y))
         self.position = self.rect.center
         self.angle = 0
-        self.max_speed = 2
+        self.max_speed = 1.5
         self.acceleration = 0
         self.speed = round(self.acceleration)
         self.dir = 0
@@ -67,11 +66,15 @@ class Spaceship(pygame.sprite.Sprite):
     def move(self,mouse_pos):
     
         dir = pygame.math.Vector2(mouse_pos) - self.position
+        dis = math.sqrt((pygame.mouse.get_pos()[1] - spaceship.position[1])**2+(pygame.mouse.get_pos()[0] -spaceship.position[0])**2 )
         self.dir = dir
+        
         if dir.length() > 60:
             dir = dir.normalize()
             self.position += dir * self.acceleration
             self.rect.center = self.position
+        
+
         
         if pygame.mouse.get_pressed()[0]:
             self.acceleration +=0.02
@@ -84,7 +87,6 @@ class Spaceship(pygame.sprite.Sprite):
             self.acceleration = 0.05
       
     def update(self):
-        self.rect.x - 100
         screen.blit(self.surf,self.rect) 
 
 class Rocket_smoke():
@@ -121,7 +123,7 @@ class Rocket_smoke():
     def update(self):
         self.x += self.spread[0]
         self.y += self.spread[1]
-        self.size -=.02
+        self.size -= random.uniform(.02,.04)
         self.lifetime -=.5
         if pygame.mouse.get_pressed()[0]:
             self.velocity = .03
@@ -136,7 +138,7 @@ class Rocket_smoke():
                 pygame.draw.circle(screen,self.colors,(self.x,self.y),self.size)
 
 class Stars():
-    def __init__(self,x,y,size,layer):
+    def __init__(self,x,y,size,layer,move):
         self.x = x 
         self.y = y
         self.dx = 0
@@ -147,38 +149,50 @@ class Stars():
         self.colors = [self.white,self.white2]
         self.color = self.white
         self.center = (self.x,self.y)
-        self.layer = layer
+        self.layer = layer 
+        self.move = move
         
        
     def update(self,angle):
-        val = self.layer 
-    
-        if (mouse_pos[0] - spaceship.position[0]) > angle:
-            dx = -val
-        elif (mouse_pos[0] - spaceship.position[0]) < -angle:
-            dx = val
-        else:
-            dx = 0
-        if (mouse_pos[1] - spaceship.position[1]) > angle:
-            dy = -val
-        elif (mouse_pos[1] - spaceship.position[1]) < -angle:
-            dy = val
-        else:
-            dy = 0
-    
-        self.x += dx
-        self.y += dy
-        self.dx = dx
-        self.dy = dy
+        offset = (self.layer +spaceship.acceleration) 
 
-        offset = pygame.math.Vector2((self.x +self.dx),(self.y+ self.dy)) 
+        if (mouse_pos[0] - spaceship.position[0]) > angle:
+            dx = -offset 
+        elif (mouse_pos[0] - spaceship.position[0]) < -angle:
+            dx = offset
+        else:
+            dx = self.move
+        if (mouse_pos[1] - spaceship.position[1]) > angle:
+            dy = -offset
+        elif (mouse_pos[1] - spaceship.position[1]) < -angle:
+            dy = offset
+        else:
+            dy = self.move
+
+        self.x += dx 
+        self.y += dy 
+        self.dx = dx 
+        self.dy = dy 
+
+        offset = pygame.math.Vector2((self.x + self.dx),(self.y+ self.dy)) 
         offset.normalize()
         self.center = offset
         return self.center
  
     def draw(self):
         pygame.draw.circle(screen,self.color,(Stars.update(self,20)),self.radius)
+    
+    def reposition(self,xylimit,respawn_dis):
+        if self.center[0] > SCREEN_WIDTH + xylimit:
+            self.x = -respawn_dis
+        elif self.center[0] < -xylimit :
+            self.x = (SCREEN_WIDTH + respawn_dis)
 
+
+        if self.center[1] > SCREEN_HEIGHT + xylimit:
+            self.y = -respawn_dis
+        elif self.center[1] < -xylimit :
+            self.y = (SCREEN_HEIGHT + respawn_dis)
 
 
 spaceship = Spaceship("xzplore/assets/spaceship.png",SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
@@ -195,11 +209,12 @@ while True:
     World_pos.dis_cal(mouse_pos,spaceship.rect.center)
     World_pos.x_offset,World_pos.y_offset = World_pos.direction(15,mouse_pos)
     
-    if len(stars) < 200:
-        stars.append(Stars(random.randint(50,SCREEN_WIDTH),random.randint(50,SCREEN_HEIGHT),random.uniform(0,5),random.uniform(0.1,20)/10))
+    if len(stars) < 150:
+        stars.append(Stars(random.randint(50,SCREEN_WIDTH),random.randint(50,SCREEN_HEIGHT),random.uniform(0,5),random.uniform(0.1,15)/10,random.uniform(-.002,.002)),)
     
     for star in stars:
         star.draw()
+        star.reposition(random.randint(100,200),random.randint(50,90))
 
     particles.append(Rocket_smoke(spaceship.rect.center[0],spaceship.rect.center[1]))
     for particle in particles[:]:
