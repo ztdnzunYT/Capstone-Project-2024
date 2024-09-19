952b305cb2934fa736ff6d9eb159914924f43427
Z Dunson <123149208+ztdnzunYT@users.noreply.github.com>
2024-09-19 08:56:40 -0500

    Update test.py

--- xzplore/test.py
+++ xzplore/test.py
@@ -14,7 +14,7 @@ pygame.display.set_caption("Basic Pygame Window")
 # Set a color
 background_color = (255, 255, 255)  # White
 
-val1 = 0 
+time = 0 
 val2 = 0 
 val3 = 0
 x = 0 
@@ -31,12 +31,14 @@ class Particle():
         self.positon = (x,y)
         self.x_max = self.x *-1
         self.speed = speed
-        self.color1 = (255, 253, 208)
-        self.color2 = (211, 211, 211)
-        self.colors = [self.color1,self.color2]
+        self.color1 = (128, 0, 0)
+        self.color2 = (255, 69, 0)
+        self.color3 = (47, 79, 79)
+        self.colors = [self.color1,self.color2,self.color3]
         self.color = random.choice(self.colors)
         self.size = size
-        print()
+        self.life_time = 0
+
 
     def draw(self):
         pygame.draw.circle(window,self.color,(self.x+300,self.y+300),self.size)
@@ -44,9 +46,13 @@ class Particle():
     def update(self):
         self.x += self.speed
         if self.x > self.x_max:
-            self.x = self.positon[0]    
+            self.x = self.positon[0] 
+        
+        self.life_time += 0.1
+        self.y += .2 *  math.sin(2*math.pi+self.life_time)
 
-        self.y += z  
+        
+
 
 particles = []
 
@@ -60,26 +66,24 @@ while running:
     window.fill((0,0,0))
 
 
-    val1 +=10
-    val2 +=10
-    val3 +=0.02
+    time +=1
+    val3 += 0.5
 
-    x = radius * math.sin(val1)
-    y = radius * math.cos(val2)
-    up = math.sin(val1)
+    x = radius * math.sin(time)
+    y = radius * math.cos(time)
+    up = math.sin(time)
 
-    z = 10 * math.sin(val3)/1.1
-    print(round(x),round(y))
-    #pygame.draw.circle(window,(255,255,255),(300,300),110,1)
+    #print(round(x),round(y))
+    #pygame.draw.circle(window,(255,255,255),(300,300),60,1)
     #pygame.draw.circle(window,(255,0,0),(x+300,y+300),size)
     #pygame.draw.circle(window,(0,0,255),(x+300,300),size)
-    pygame.draw.circle(window,(0,255,0),(300,z+300),size)
+    #pygame.draw.circle(window,(0,255,0),(300,z+300),size)
     
 
     
-    if len(particles) < 12000:
+    if len(particles) < 6000:
         if x < 0:
-            particles.append(Particle(x ,y,1,random.uniform(1,1)))
+            particles.append(Particle(x ,y,random.uniform(1,2),random.uniform(1,2.5)))
 
     for part in particles:
         part.draw()
